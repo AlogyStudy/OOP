1a678e87fc409933c0e66a2b037f6e484d8b5d9b
Xingzhang Lin <1129507496@qq.com>
2016-06-12 21:09:47 +0800

    js接口

---
+++ designer/obersver/a.js
@@ -0,0 +1,14 @@
+
+/**
+ * 消息模块
+ */
+
+(function () {
+	
+	var dom = $('msg_num');
+	
+	Observer.add('addMsg',function () {
+		
+	});
+	
+})();
---
+++ designer/obersver/obersver.js
@@ -0,0 +1,84 @@
+/**
+ * 订阅模式
+ */
+			
+var Observer = (function () {
+	
+	var __msg = {};
+	
+	return {
+		
+		/**
+	 * 添加订阅者 
+	 * @param {String} type  订阅者名字
+	 * @param {Function} fn  执行函数  
+	 */
+	add: function ( type,fn ) {
+		
+		if ( __msg[type] ) {
+			
+			__msg[type].push(fn);
+			
+		}	else {
+			
+			__msg[type] = [fn];
+			
+		}
+		
+		return this;
+		
+	},
+	
+	/**
+	 * 执行回调函数
+	 * @param {String} type
+	 */
+	fire: function ( type,data ) {
+		
+		if ( __msg[type] ) {
+			
+			var e = data
+			
+			for ( var i=0; i<__msg[type].length; ++i ) {
+				
+				__msg[type][i](e);
+				
+			}
+			
+		}
+		
+		return this;
+		
+	},
+	
+	/**
+	 * 移除回调函数
+	 * @param {String} type
+	 * @param {String} fnName
+	 */
+		remove: function ( type,fnName ) {
+			
+			if ( __msg[type] ) {
+				
+				for ( var i = __msg[type].length-1; i>=0; --i ) {
+					
+					if ( __msg[type][i] === fnName ) {
+						
+						__msg[type].splice(i,1);
+						
+					}
+					
+				}
+				
+			}
+			
+			return this;
+		}
+		
+	}
+	
+	function $ ( id ) {
+		return document.getElementById(id);
+	}
+	
+})()
