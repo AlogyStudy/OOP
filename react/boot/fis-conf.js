ac803a931c9bbd309907c60c799fc4b871cb2dad
Xingzhang Lin <1129507496@qq.com>
2016-08-06 23:38:37 +0800

    study react

--- react/boot/fis-conf.js
+++ react/boot/fis-conf.js
@@ -1,5 +1,6 @@
 
 
+// 编译Less
 fis.match('**.less', {
 	
 	parser: fis.plugin('less'),
@@ -9,7 +10,19 @@ fis.match('**.less', {
 });
 
 
+// 编译JSX
 fis.match('**.jsx', {
+	
+	parser: fis.plugin('babel2'),
+	
+	rExt: '.js'
+	
+});
+
+
+// 编译HTML中的JSX
+
+fis.match('**.html:jsx', {
 	
 	parser: fis.plugin('babel2'),
 	
